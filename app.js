de91ce3c21c808adb53f307ae29ce4bfee9ea5e3
nitin.bhatta0225@gmail.com <nitin.bhatta0225@gmail.com>
2024-11-14 11:48:45 +1100

    "Added initial implementation of Student Grade Calculator with HTML, CSS, and JavaScript files"

---
+++ app.js
@@ -0,0 +1,14 @@
+let maths = 80;
+let science = 70;
+let computer = 60;
+
+let average = (maths + science + computer) / 3;
+console.log("The average is", average);
+
+if (average >= 80) {
+  console.log("Grade A");
+} else if (average >= 60) {
+  console.log("Grade B");
+} else {
+  console.log("Grade C");
+}
